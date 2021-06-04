368414350da99709480c1810c84545987a2b0b84
rinka0v0 <rinka0v0@users.noreply.github.com>
2021-06-05 00:23:33 +0900

    [update] Footerコンポーネントに内容を追加
    利用規約とプライバシーポリシーのリンクの追加

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,10 +1,20 @@
-import { Box } from "@chakra-ui/layout";
+import { Box, Flex } from "@chakra-ui/layout";
+import Link from "next/link";
 import React from "react";
 
 const Footer: React.VFC = () => {
   return (
     <Box as="footer">
       <Box textAlign="center">R_MUSEUM</Box>
+      <Flex justify="">
+        <Box mx='auto'>
+          <Link href="/">利用規約</Link>
+          <Link href="/">プライバシーポリシー</Link>
+        </Box>
+      </Flex>
+      <Box textAlign="center" fontSize="sm">
+        &copy; since 2021 rinka All rights reserved
+      </Box>
     </Box>
   );
 };
