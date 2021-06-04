1b32b60119fa70ec866da43e853c3fd3a64a9c0e
rinka0v0 <rinka0v0@users.noreply.github.com>
2021-06-04 22:22:23 +0900

    [add] フッターコンポーネントの追加

---
+++ components/layout/Footer.tsx
@@ -0,0 +1,11 @@
+import { Box } from "@chakra-ui/layout";
+
+const Footer = () => {
+  return (
+    <Box as="footer">
+      <Box textAlign="center">R_MUSEUM</Box>
+    </Box>
+  );
+};
+
+export default Footer
