09a3961e7b98b7045a3acf7c9d91199269c3682a
rinka0v0 <rinka0v0@users.noreply.github.com>
2021-06-04 22:27:42 +0900

    [add] Headerコンポーネントの追加
    レスポンシブにも対応した

---
+++ components/layout/Header.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import NextLink from "next/link";
+import { IconButton } from "@chakra-ui/button";
+import { SearchIcon } from "@chakra-ui/icons";
+import { Box, Flex } from "@chakra-ui/layout";
+import { Avatar } from "@chakra-ui/avatar";
+import PrimaryButton from "../atoms/button/PrimaryButton";
+import Link from "next/link";
+
+type Props = {
+  isLogin: boolean;
+};
+
+const Header: React.VFC<Props> = (props) => {
+  const { isLogin } = props;
+  return (
+    <Flex
+      as="nav"
+      bg="white"
+      align="center"
+      justify="space-between"
+      height="60px"
+    >
+      <Box ml={3} fontSize="lg" fontWeight="bold">
+        <Link href="/">R_MUSEUM</Link>
+      </Box>
+      <Flex align="center" mr={3}>
+        <NextLink href="/serch">
+          <IconButton
+            backgroundColor="#ddd"
+            aria-label="Search database"
+            icon={<SearchIcon />}
+            mr={3}
+          />
+        </NextLink>
+        {isLogin ? (
+          <>
+            <NextLink href="/post">
+              <PrimaryButton>寄贈する</PrimaryButton>
+            </NextLink>
+            <Avatar src="https://bit.ly/broken-link" mr={3} ml={3} />
+          </>
+        ) : (
+          <PrimaryButton>ログイン</PrimaryButton>
+        )}
+      </Flex>
+    </Flex>
+  );
+};
+
+export default Header;
