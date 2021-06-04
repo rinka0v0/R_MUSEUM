12d279704f578f3c2329180011151dfd9ddee9d2
rinka0v0 <rinka0v0@users.noreply.github.com>
2021-06-04 22:32:09 +0900

    [add] ダッシュボードページの追加
    投稿を管理できるページを追加しただけ、機能は実装していない

---
+++ pages/dashboard/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../../components/layout/Header";
+
+const Dashbord: React.VFC = () => {
+  return (
+    <>
+      <Header isLogin={true} />
+      <div>Dashbord page</div>
+    </>
+  );
+};
+
+export default Dashbord;
