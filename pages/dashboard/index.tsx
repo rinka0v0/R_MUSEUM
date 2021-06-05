50d10668aa534e574f6b46f2ca1fa23167e55ec2
rinka0v0 <rinka0v0@users.noreply.github.com>
2021-06-05 21:53:30 +0900

    [update] dashboardページに作品一覧を仮のデザインで表示
    コンポーネント化はまだできていない

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,3 +1,4 @@
+import { Box, Flex, Heading } from "@chakra-ui/layout";
 import React from "react";
 import Header from "../../components/layout/Header";
 
@@ -5,7 +6,16 @@ const Dashbord: React.VFC = () => {
   return (
     <>
       <Header isLogin={true} />
-      <div>Dashbord page</div>
+      <Flex flexDirection="column" alignItems="center">
+        <Heading fontSize={20} my={5}>自分の作品一覧</Heading>
+        <Box border="1px solid #ddd" w={{base: '300px', md:'500px'}} p={3} borderRadius={5} bg='white'>
+          <Box>title</Box>
+          <Flex justify="space-around">
+            <Box color="red">公開中</Box>
+            <Box>10日前</Box>
+          </Flex>
+        </Box>
+      </Flex>
     </>
   );
 };
