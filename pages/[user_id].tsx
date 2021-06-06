f79812658294584c4fa62980ac968b6b6cebcd76
rinka0v0 <rinka0v0@users.noreply.github.com>
2021-06-06 23:21:47 +0900

    [add] ユーザーページの追加
    mypageとこのユーザーページを分ける必要ない可能性が出てきた

---
+++ pages/[user_id].tsx
@@ -0,0 +1,42 @@
+import { Avatar } from "@chakra-ui/avatar";
+import { Box, Flex, Heading } from "@chakra-ui/layout";
+import React from "react";
+import Footer from "../components/layout/Footer";
+import Header from "../components/layout/Header";
+
+const ProductPage: React.VFC = () => {
+  return (
+    <>
+      <Header isLogin={false} />
+      <Flex alignItems="center" flexDirection="column" maxH="1000px">
+        <Flex
+          w={{ md: "90%" }}
+          alignItems="center"
+          my={10}
+          justify="space-around"
+          flexDirection={{ base: "column", md: "row" }}
+        >
+          <Avatar
+            src="https://bit.ly/broken-link"
+            size="2xl"
+            mr={3}
+            ml={3}
+            mt={5}
+          />
+          <Box width="70%">
+            <Box fontSize={30}>rinka</Box>
+            <Box as="p" fontSize={{ base: ".95em", md: "16px" }}>
+              自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介
+            </Box>
+          </Box>
+        </Flex>
+        <Heading fontSize={24} textAlign="center">
+          rinkaさんの作品
+        </Heading>
+      </Flex>
+      <Footer />
+    </>
+  );
+};
+
+export default ProductPage;
