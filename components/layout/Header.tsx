ce2509df68b6024b631cfe03882591837115044f
rinka0v0 <rinka0v0@users.noreply.github.com>
2021-06-07 13:14:50 +0900

    [update] メニュー表示の追加
    アイコンをクリックするとメニューが表示されるようにした。

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -8,6 +8,14 @@ import PrimaryButton from "../atoms/button/PrimaryButton";
 import Link from "next/link";
 
 import { nanoid } from "nanoid";
+import {
+  Popover,
+  PopoverBody,
+  PopoverCloseButton,
+  PopoverContent,
+  PopoverHeader,
+  PopoverTrigger,
+} from "@chakra-ui/popover";
 
 type Props = {
   isLogin: boolean;
@@ -45,9 +53,27 @@ const Header: React.VFC<Props> = (props) => {
             <NextLink href={`/products/${nanoid()}/edit`}>
               <PrimaryButton onClick={onClickBtn}>寄贈する</PrimaryButton>
             </NextLink>
-            <NextLink href="/mypage">
-              <Avatar src="https://bit.ly/broken-link" mr={3} ml={3} />
-            </NextLink>
+
+            <Popover>
+              <PopoverTrigger>
+                <Avatar src="https://bit.ly/broken-link" mx={3} />
+              </PopoverTrigger>
+              <PopoverContent w="150px" mr={5}>
+                <PopoverCloseButton />
+                <PopoverHeader>Menu</PopoverHeader>
+                <PopoverBody p={0}>
+                  <Box  border="1px solid #ddd" h='30px'>
+                    <Link href="/mypage">マイページ</Link>
+                  </Box>
+                  <Box border="1px solid #ddd" h='30px'>
+                    <Link href="/dashboard">作品の管理</Link>
+                  </Box>
+                  <Box border="1px solid #ddd" h='30px'>
+                    <Link href="">サインアウト</Link>
+                  </Box>
+                </PopoverBody>
+              </PopoverContent>
+            </Popover>
           </>
         ) : (
           <PrimaryButton>ログイン</PrimaryButton>
