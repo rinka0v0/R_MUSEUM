06a859ce2a701eda8413bac4f76e0c33d7c92e6a
rinka0v0 <rinka0v0@users.noreply.github.com>
2021-06-08 22:56:18 +0900

    [add] プライバシーポリシー,利用規約ページの追加　
    内容はまだなし

---
+++ pages/privacy.tsx
@@ -0,0 +1,12 @@
+import { Box } from "@chakra-ui/layout";
+import React from "react";
+
+const Privacy: React.VFC = () => {
+  return (
+    <Box >
+      プライバシー権
+    </Box>
+  );
+};
+
+export default Privacy;
---
+++ pages/terms.tsx
@@ -0,0 +1,8 @@
+import { Box } from "@chakra-ui/layout";
+import React from "react";
+
+const Terms: React.VFC = () => {
+  return <Box>利用規約</Box>;
+};
+
+export default Terms;
