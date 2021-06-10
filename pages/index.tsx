7d2002f19528dc6a4c02b70da6a7104b9016bcf4
rinka0v0 <rinka0v0@users.noreply.github.com>
2021-06-10 23:17:39 +0900

    [update] 不必要なコードを削除

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,41 +1,16 @@
-import React, { useState, useEffect } from "react";
-import TaskItem from "../components/TaskItem";
-import { auth, db } from "../firebase";
+import React, { useContext } from "react";
 import Header from "../components/layout/Header";
 import TrendLanguage from "../components/card/TrendLanguage";
 import Exhibit from "../components/card/Exhibit";
-import PrimaryButton from "../components/atoms/button/PrimaryButton";
 import { Box, Flex, Heading, VStack } from "@chakra-ui/layout";
+import { AuthContext } from "../auth/AuthProvider";
 
 const IndexPage: React.VFC = () => {
-  const [tasks, setTasks] = useState([{ id: "", title: "" }]);
-  const [input, setInput] = useState("");
-  const [isLogin, setIsLogin] = useState(false);
-
-  useEffect(() => {
-    const unSub = auth.onAuthStateChanged((user) => {
-      !user && "ログインされていません";
-    });
-    return () => unSub();
-  }, []);
-
-  useEffect(() => {
-    const unSub = db.collection("tasks").onSnapshot((snapshot) => {
-      setTasks(
-        snapshot.docs.map((doc) => ({ id: doc.id, title: doc.data().title }))
-      );
-      return () => unSub();
-    });
-  }, []);
-
-  const newTask = (e: any) => {
-    db.collection("tasks").add({ title: input });
-    setInput("");
-  };
+  const { currentUser } = useContext(AuthContext);
 
   return (
     <Box>
-      <Header isLogin={isLogin} />
+      <Header isLogin={currentUser ? true : false} />
       <Flex align="center" justify="center" flexDirection="column">
         <VStack w="100%" spacing={8}>
           <Box w="80%" mt={10}>
@@ -46,7 +21,7 @@ const IndexPage: React.VFC = () => {
           </Heading>
           <Exhibit
             exhibit={{
-              name: "name",
+              name: "Go で作る....",
               userName: "rinka",
               userIcon: "",
               likes: 0,
@@ -62,29 +37,6 @@ const IndexPage: React.VFC = () => {
               createdAt: "10日前",
             }}
           />
-
-          <PrimaryButton
-            onClick={() => {
-              setIsLogin(!isLogin);
-            }}
-          >
-            ログイン
-          </PrimaryButton>
-          {/* <button
-            onClick={async () => {
-              try {
-                await auth.signOut();
-                console.log("please login");
-              } catch (error) {
-                alert(error.message);
-              }
-            }}
-          >
-            LOG OUT
-          </button> */}
-          {/* <button disabled={!input} onClick={newTask}>
-            SEND
-          </button> */}
         </VStack>
       </Flex>
     </Box>
