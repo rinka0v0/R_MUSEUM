a771a1eb0f48048bbc8527e365e584e8971af410
rinka0v0 <rinka0v0@users.noreply.github.com>
2021-06-10 23:16:23 +0900

    [update] 認証情報を管理するためプロバイダーの設定をした

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,22 +3,24 @@ import type { AppProps } from "next/app";
 import Head from "next/head";
 import Footer from "../components/layout/Footer";
 import theme from "../theme/theme";
+import { AuthProvider } from "../auth/AuthProvider";
 
 const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
   return (
     <>
-      <ChakraProvider theme={theme}>
-        <Head>
-          <title>R_MUSEUM</title>
-        </Head>
-        <Flex minH="100vh" flexDirection="column">
-          <Box flex='1'>
-
-          <Component {...pageProps} />
-          </Box>
-          <Footer />
-        </Flex>
-      </ChakraProvider>
+      <AuthProvider>
+        <ChakraProvider theme={theme}>
+          <Head>
+            <title>R_MUSEUM</title>
+          </Head>
+          <Flex minH="100vh" flexDirection="column">
+            <Box flex="1">
+              <Component {...pageProps} />
+            </Box>
+            <Footer />
+          </Flex>
+        </ChakraProvider>
+      </AuthProvider>
     </>
   );
 };
