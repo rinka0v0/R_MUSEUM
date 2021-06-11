e4f9df14f380195192023128a19cf1a9a785bc89
rinka0v0 <rinka0v0@users.noreply.github.com>
2021-06-11 11:45:35 +0900

    [update] Headerコンポーネントを表示させた

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,23 +4,25 @@ import Head from "next/head";
 import Footer from "../components/layout/Footer";
 import theme from "../theme/theme";
 import { AuthProvider } from "../auth/AuthProvider";
+import Header from "../components/layout/Header";
 
 const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
   return (
     <>
-      <AuthProvider>
-        <ChakraProvider theme={theme}>
+      <ChakraProvider theme={theme}>
+        <AuthProvider>
           <Head>
             <title>R_MUSEUM</title>
           </Head>
           <Flex minH="100vh" flexDirection="column">
+            <Header />
             <Box flex="1">
               <Component {...pageProps} />
             </Box>
             <Footer />
           </Flex>
-        </ChakraProvider>
-      </AuthProvider>
+        </AuthProvider>
+      </ChakraProvider>
     </>
   );
 };
