d6eac92ded3e7a99845f844f18ffefcfdd8bac45
rinka0v0 <rinka0v0@users.noreply.github.com>
2021-06-17 23:51:06 +0900

    [update] レイアウトの修正

--- components/Input/TagsInput.tsx
+++ components/Input/TagsInput.tsx
@@ -1,12 +1,11 @@
 /**@jsxRuntime classic */
 /** @jsx jsx */
 
-import React, { useState } from "react";
+import { useState } from "react";
 import { VFC } from "react";
 import { jsx, css } from "@emotion/react";
-import { Box } from "@chakra-ui/layout";
-
-import { ListItem, UnorderedList, Button, Input } from "@chakra-ui/react";
+import { Box, Flex } from "@chakra-ui/layout";
+import { ListItem, UnorderedList, Input } from "@chakra-ui/react";
 
 const TagInput: VFC = () => {
   const [tags, setTags] = useState<string[]>([]);
@@ -31,16 +30,27 @@ const TagInput: VFC = () => {
   };
 
   return (
-    <>
-      <Box>
-        <UnorderedList>
+    <Flex
+      alignItems="center"
+      justify="center"
+      boxSizing="border-box"
+      css={container}
+    >
+      <Box className="tags-input">
+        <UnorderedList id="tags">
           {tags.map((tag, index) => {
             return (
-              <ListItem key={tag}>
-                {tag}
-                <Button type="button" onClick={() => removeTag(index)}>
+              <ListItem key={tag} className="tag">
+                <Box as="span" className="tag-title">
+                  {tag}
+                </Box>
+                <Box
+                  as="span"
+                  className="tag-close-icon"
+                  onClick={() => removeTag(index)}
+                >
                   ✖️
-                </Button>
+                </Box>
               </ListItem>
             );
           })}
@@ -48,10 +58,84 @@ const TagInput: VFC = () => {
         <Input
           placeholder="使用した技術"
           onKeyUp={(e) => inputKeyDown(e)}
+          flex="1"
+          h="46px"
+          padding="4px 0 0 0"
+          _focus={{ outline: "transparent" }}
         ></Input>
       </Box>
-    </>
+    </Flex>
   );
 };
+
+const container = css`
+  .tags-input {
+    display: flex;
+    align-items: flex-start;
+    flex-wrap: wrap;
+    min-height: 48px;
+    // width: 480px;
+    padding: 0 8px;
+    border: 1px solid rgb(214, 216, 218);
+    border-radius: 6px;
+    &:focus-within {
+      border: 1px solid #0052cc;
+    }
+    input {
+      flex: 1;
+      border: none;
+      height: 46px;
+      font-size: 14px;
+      padding: 4px 0 0 0;
+      &:focus {
+        outline: transparent;
+      }
+    }
+  }
+
+  #tags {
+    display: flex;
+    flex-wrap: wrap;
+    padding: 0;
+    margin: 8px 0 0 0;
+  }
+
+  .tag {
+    width: auto;
+    height: 32px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: #fff;
+    padding: 0 8px;
+    font-size: 14px;
+    list-style: none;
+    border-radius: 6px;
+    margin: 0 8px 8px 0;
+    background: #0052cc;
+    .tag-title {
+      margin-top: 3px;
+    }
+    .tag-close-icon {
+      display: block;
+      width: 16px;
+      height: 16px;
+      line-height: 16px;
+      text-align: center;
+      font-size: 14px;
+      margin-left: 8px;
+      color: #0052cc;
+      border-radius: 50%;
+      background: #fff;
+      cursor: pointer;
+    }
+  }
+
+  @media screen and (max-width: 567px) {
+    .tags-input {
+      width: calc(100vw - 32px);
+    }
+  }
+`;
 
 export default TagInput;
