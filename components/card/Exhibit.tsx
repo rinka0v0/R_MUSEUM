4afe0800d5e20a2bd5806e5d5036f115985f1ab8
rinka0v0 <rinka0v0@users.noreply.github.com>
2021-06-25 12:06:24 +0900

    [update] propsから受け取ったユーザーの名前とアイコンを表示するように変更

--- components/card/Exhibit.tsx
+++ components/card/Exhibit.tsx
@@ -18,14 +18,27 @@ type Props = {
 const Exhibit: React.VFC<Props> = (props) => {
   const { exhibit } = props;
   return (
-    <Link href={`/products/${exhibit.id}`} >
-      <Box w={350} backgroundColor="white" borderRadius="lg" p={3} cursor='pointer'>
+    <Link href={`/products/${exhibit.id}`}>
+      <Box
+        w={350}
+        backgroundColor="white"
+        borderRadius="lg"
+        p={3}
+        cursor="pointer"
+      >
         <Heading as="h1" fontSize="md" minH="30px">
           {exhibit.name}
         </Heading>
         <Flex justify="space-between" align="center" mt={3}>
           <Flex alignItems="center">
-            <Avatar src="https://bit.ly/broken-link" mr={3} />
+            <Avatar
+              src={
+                exhibit.userIcon
+                  ? exhibit.userIcon
+                  : "https://bit.ly/broken-link"
+              }
+              mr={3}
+            />
             <Box>{exhibit.userName}</Box>
           </Flex>
           {exhibit.sourceCode ? <Box bg="teal">コード公開中</Box> : null}
