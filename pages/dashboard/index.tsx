50f107a9041710e4a0c313c3294f86cb7de87303
rinka0v0 <rinka0v0@users.noreply.github.com>
2021-06-28 22:58:53 +0900

    [update] 投稿一覧を取得し表示する

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,16 +1,51 @@
+import React, { useContext, useEffect } from "react";
 import Router from "next/router";
 import { Box, Flex, Heading } from "@chakra-ui/layout";
-import React, { useContext, useEffect } from "react";
 import Header from "../../components/layout/Header";
 import Loading from "../../components/layout/Loading";
 import { AuthContext } from "../../auth/AuthProvider";
+import DashBoard from "../../components/card/DashBoard";
+import { db } from "../../firebase";
+import { useState } from "react";
+import moment from "moment";
+import { Stack } from "@chakra-ui/react";
+
+type ProductData = {
+  id: string;
+  title: string;
+  createdAt: string;
+  isOpen: boolean;
+};
 
 const Dashbord: React.VFC = () => {
   const { currentUser, signInCheck } = useContext(AuthContext);
+  const [products, setProducts] = useState<Array<ProductData>>([]);
+
+  const fetchProducts = async () => {
+    await db
+      .collection("products")
+      .where("userId", "==", currentUser?.uid)
+      .get()
+      .then((products) => {
+        const productList: Array<ProductData> = [];
+        products.forEach((product) => {
+          const data = product.data();
+          const date: string = data.createdAt.toDate().toString();
+          productList.push({
+            id: product.id,
+            title: data.title,
+            createdAt: moment(date).fromNow(),
+            isOpen: data.open,
+          });
+        });
+        setProducts(productList);
+      });
+  };
 
   useEffect(() => {
     !currentUser && Router.push("/");
-  }, [currentUser]);
+    fetchProducts();
+  }, []);
 
   if (!signInCheck || !currentUser) {
     return <Loading />;
@@ -22,19 +57,20 @@ const Dashbord: React.VFC = () => {
         <Heading fontSize={20} my={5}>
           自分の作品一覧
         </Heading>
-        <Box
-          border="1px solid #ddd"
-          w={{ base: "300px", md: "500px" }}
-          p={3}
-          borderRadius={5}
-          bg="white"
-        >
-          <Box>title</Box>
-          <Flex justify="space-around">
-            <Box color="red">公開中</Box>
-            <Box>10日前</Box>
-          </Flex>
-        </Box>
+        <Stack spacing={5}>
+          {products.map((product) => {
+            return (
+              <Box key={product.id} cursor="pointer">
+                <DashBoard
+                  url={`/products/${product.id}/edit`}
+                  isOpen={product.isOpen}
+                  title={product.title}
+                  createdAt={product.createdAt}
+                />
+              </Box>
+            );
+          })}
+        </Stack>
       </Flex>
     </>
   );
