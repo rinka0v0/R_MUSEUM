cbb929ade58c2c87bff4e3bcc29c17046f6b8bcf
rinka0v0 <rinka0v0@users.noreply.github.com>
2021-06-28 12:22:03 +0900

    [update] ユーザー情報をfirestoreからとってくる
    レイアウトの部分はまだやっていない

--- pages/mypage/index.tsx
+++ pages/mypage/index.tsx
@@ -7,12 +7,62 @@ import { AuthContext } from "../../auth/AuthProvider";
 import PrimaryButton from "../../components/atoms/button/PrimaryButton";
 import Loading from "../../components/layout/Loading";
 import Header from "../../components/layout/Header";
+import { useState } from "react";
+import { db } from "../../firebase";
+import Exhibit from "../../components/card/Exhibit";
+import moment from "moment";
+
+type User = {
+  name: string;
+  iconURL: string;
+  profile: string;
+  products?: Array<any>;
+};
 
 const Mypage: React.VFC = () => {
   const { currentUser, signInCheck } = useContext(AuthContext);
+  console.log(currentUser?.uid);
+  const [user, setUser] = useState<User | undefined>({
+    name: "",
+    iconURL: "",
+    profile: "",
+    products: [],
+  });
+
+  const fetchUser = async () => {
+    await db
+      .collection("users")
+      .doc(currentUser?.uid)
+      .get()
+      .then(async (userData) => {
+        const user = await userData.data();
+        return user;
+      })
+      .then(async (user) => {
+        await db
+          .collection("products")
+          .where("userId", "==", currentUser?.uid)
+          .get()
+          .then((products) => {
+            const productsList: Array<any> = [];
+            products.forEach((doc) => {
+              productsList.push(doc.data());
+            });
+            const userObject = {
+              name: user?.user_name,
+              iconURL: user?.iconURL,
+              profile: user?.profile,
+              products: productsList,
+            };
+            setUser(userObject);
+          });
+      });
+  };
 
   useEffect(() => {
     !currentUser && Router.push("/");
+    fetchUser();
+    console.log(user, "useEffect");
   }, [currentUser]);
 
   if (!signInCheck || !currentUser) {
@@ -52,13 +102,33 @@ const Mypage: React.VFC = () => {
           <Box width="70%">
             <Box fontSize={30}>{currentUser.displayName}</Box>
             <Box as="p" fontSize={{ base: ".95em", md: "16px" }}>
-              自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介
+              {user?.profile}
             </Box>
           </Box>
         </Flex>
         <Heading fontSize={24} textAlign="center">
           {currentUser.displayName}さんの作品
         </Heading>
+        <Box>
+          {user?.products
+            ? user?.products.map((product, index) => {
+                const date: string = product.createdAt.toDate().toString();
+                return (
+                  <Exhibit
+                    key={index}
+                    exhibit={{
+                      id: product.id,
+                      name: product.title,
+                      userName: user.name,
+                      userIcon: user.iconURL,
+                      likes: 0,
+                      createdAt: moment(date).fromNow(),
+                    }}
+                  />
+                );
+              })
+            : null}
+        </Box>
       </Flex>
       <></>
     </>
