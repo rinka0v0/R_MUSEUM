6b54ae1c5567ec41e8490f0fdcff6e5deba9e2e4
rinka0v0 <rinka0v0@users.noreply.github.com>
2021-07-12 21:10:37 +0900

    [update] テストの追加
    productsドキュメントのテストの追加

--- firestore.rules.test.ts
+++ firestore.rules.test.ts
@@ -34,13 +34,13 @@ describe("Firestore rulesのテスト", () => {
   });
 
   describe("usersコレクションへの認証アクセスを許可", () => {
-    test("認証ユーザーでのデータ保存に成功", async () => {
+    test("認証ユーザーでのユーザー情報の登録", async () => {
       const db = getFirestoreWithAuth();
       const doc = db.collection("users").doc("test");
       await firebase.assertSucceeds(
         doc.set(
           {
-            // createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+            createdAt: firebase.firestore.FieldValue.serverTimestamp(),
             gitHub: "dd",
             iconURL: "https://...",
             instagram: "dd",
@@ -52,12 +52,11 @@ describe("Firestore rulesのテスト", () => {
         )
       );
     });
-    test("認証済みのアカウントでユーザー情報を更新", async () => {
+    test("認証ユーザーでの情報を更新", async () => {
       const db = getFirestoreWithAuth();
       const doc = db.collection("users").doc("test");
       await firebase.assertSucceeds(
         doc.update({
-          // createdAt: firebase.firestore.FieldValue.serverTimestamp(),
           gitHub: "dd",
           iconURL: "https://...",
           instagram: "dd",
@@ -67,25 +66,120 @@ describe("Firestore rulesのテスト", () => {
         })
       );
     });
+
     test("認証済みのアカウントでユーザー情報をget", async () => {
       const db = getFirestoreWithAuth();
       const doc = db.collection("users").doc("test");
       await firebase.assertSucceeds(doc.get());
     });
+
+    test("認証ユーザーで他のユーザー情報の更新", async () => {
+      const db = getFirestoreWithAuth();
+      const doc = db.collection("users").doc("alice");
+      await firebase.assertFails(
+        doc.update({
+          gitHub: "dd",
+          iconURL: "https://...",
+          instagram: "dd",
+          profile: "hey! I'm alice!",
+          twitter: "dd",
+          user_name: "alice",
+        })
+      );
+    });
+
+    test("認証ユーザーでユーザー情報の削除", async () => {
+      const db = getFirestoreWithAuth();
+      const doc = db.collection("users").doc("test");
+      await firebase.assertSucceeds(doc.delete());
+    });
+
+    test("認証ユーザーで他のユーザー情報の削除", async () => {
+      const db = getFirestoreWithAuth();
+      const doc = db.collection("users").doc("alice");
+      await firebase.assertFails(doc.delete());
+    });
+
+    test("未認証のアカウントでユーザー情報のget", async () => {
+      const db = getFirestore();
+      const doc = db.collection("users").doc("test");
+      await firebase.assertFails(doc.get());
+    });
   });
+
   describe("Products コレクション", () => {
+    test("認証ユーザーで投稿情報のget", async () => {
+      const db = getFirestore();
+      const doc = db.collection("products").doc("test");
+      await firebase.assertFails(doc.get());
+    });
+
     test("承認済みのアカウントでユーザー情報をcreate", async () => {
       const db = getFirestoreWithAuth();
-      const doc = db.collection('products').doc('test')
-      await firebase.assertSucceeds(doc.set({
-        content: '#Hello',
-        createdAt: firebase.firestore.FieldValue.serverTimestamp(),
-        open: false,
-        sorceCode: '',
-        tagsIDs: [],
-        title: 'Next.jsで作る...',
-        userId: 'test'
-      },{merge: true}))
+      const doc = db.collection("products").doc("test");
+      await firebase.assertSucceeds(
+        doc.set(
+          {
+            content: "#Hello",
+            createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+            open: false,
+            sorceCode: "",
+            tagsIDs: [],
+            title: "Next.jsで作る...",
+            userId: "test",
+          },
+          { merge: true }
+        )
+      );
     });
+
+    test("承認済みのアカウントで投稿情報をupdate", async () => {
+      const db = getFirestoreWithAuth();
+      const doc = db.collection("products").doc("test");
+      await firebase.assertSucceeds(
+        doc.set(
+          {
+            content: "#Hello",
+            open: false,
+            sorceCode: "",
+            tagsIDs: [],
+            title: "Next.jsで作る...",
+            userId: "test",
+          },
+          { merge: true }
+        )
+      );
+    });
+
+    test("他の人の投稿をupdate", async () => {
+      const db = getFirestoreWithAuth();
+      const doc = db.collection("products").doc("alice");
+      await firebase.assertFails(
+        doc.set(
+          {
+            content: "#Hello",
+            open: false,
+            sorceCode: "",
+            tagsIDs: [],
+            title: "TypeScriptで作る...",
+            userId: "alice",
+          },
+          { merge: true }
+        )
+      );
+    });
+
+    test("他の人の投稿の削除", async () => {
+      const db = getFirestoreWithAuth();
+      const doc = db.collection("products").doc("alice");
+      await firebase.assertFails(doc.delete());
+    });
+
+    test("自分の投稿の削除", async () => {
+      const db = getFirestoreWithAuth();
+      const doc = db.collection("products").doc("test");
+      await firebase.assertSucceeds(doc.delete());
+    });
+
   });
 });
