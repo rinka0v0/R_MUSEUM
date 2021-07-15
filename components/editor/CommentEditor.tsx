41c7e6e2af7ec6ac27d2e6a10fa6e6394c5caeb3
rinka0v0 <rinka0v0@users.noreply.github.com>
2021-07-15 23:11:43 +0900

    [update] コメント投稿機能の追加

--- components/editor/CommentEditor.tsx
+++ components/editor/CommentEditor.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import dynamic from "next/dynamic";
 import { Box, Heading } from "@chakra-ui/layout";
 import "easymde/dist/easymde.min.css";
@@ -7,8 +7,12 @@ import marked from "marked";
 import highlightjs from "highlight.js";
 import "highlight.js/styles/github.css";
 import "github-markdown-css";
-import { Flex } from "@chakra-ui/react";
+import { Button, Flex } from "@chakra-ui/react";
 import PrimaryButton from "../atoms/button/PrimaryButton";
+import { db } from "../../firebase";
+import firebase from "firebase";
+import { AuthContext } from "../../auth/AuthProvider";
+import useMessage from "../../hooks/useMessage";
 
 // クライアント側でインポートする必要がある
 const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
@@ -18,6 +22,7 @@ const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
 type Props = {
   markdown: string;
   HTML: string;
+  productId: string;
   setMarkdown: React.Dispatch<React.SetStateAction<string>>;
   setHTML: React.Dispatch<React.SetStateAction<string>>;
 };
@@ -34,14 +39,38 @@ marked.setOptions({
 });
 
 const CommentEditor: React.VFC<Props> = (props) => {
-  const { markdown, HTML, setMarkdown, setHTML } = props;
+  const { markdown, HTML, setMarkdown, setHTML, productId } = props;
   const [isMarkdown, setIsMarkdown] = useState(true);
+
+  const { currentUser } = useContext(AuthContext);
+  const { showMessage } = useMessage();
 
   const onclickMarkdown = () => {
     setIsMarkdown(true);
   };
   const onclickPrewview = () => {
     setIsMarkdown(false);
+  };
+
+  const postComment = () => {
+    if (markdown) {
+      db.collection("products")
+        .doc(productId)
+        .collection("comments")
+        .doc()
+        .set({
+          userId: currentUser?.uid,
+          content: markdown,
+          likes: 0,
+          createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+        })
+        .then(() => {
+          setMarkdown('')
+          showMessage({ title: "コメントしました！", status: "success" });
+        });
+    } else {
+      showMessage({ title: "コメントできませんでした", status: "error" });
+    }
   };
 
   // const handleDrop = (data: CodeMirror.Editor, e: DragEvent) => {
@@ -62,13 +91,14 @@ const CommentEditor: React.VFC<Props> = (props) => {
         <Box w="100%">
           <SimpleMDE
             value={markdown}
-            placeholder="コメントをかいてみよう"
+            placeholder="コメントを書いてみよう"
             onChange={(e: string) => {
               setMarkdown(e);
               setHTML(DOMPurify.sanitize(marked(e)));
             }}
             // events={{ drop: handleDrop }}
           />
+          <PrimaryButton onClick={postComment}>投稿</PrimaryButton>
         </Box>
       ) : (
         <Box w="100%" borderRadius={5} minH="330px" border="1px solid ">
