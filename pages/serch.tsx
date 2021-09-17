255b1f90b3b079089a568012819cb07365119e88
rinka0v0 <rinka0v0@users.noreply.github.com>
2021-09-17 20:47:59 +0900

    [update] 機能追加
    タグで検索ができるようになった（キーワード検索はできない）

--- pages/serch.tsx
+++ pages/serch.tsx
@@ -1,26 +1,81 @@
-import { Input } from "@chakra-ui/input";
-import { Flex } from "@chakra-ui/layout";
 import React, { useState } from "react";
-import PrimaryButton from "../components/atoms/button/PrimaryButton";
+import Link from "next/link";
+import { Flex } from "@chakra-ui/layout";
+import { Box, Heading } from "@chakra-ui/react";
 import Header from "../components/layout/Header";
 
 const SerchPage: React.VFC = () => {
-  const [keyWord, setKeyWord] = useState("");
+  // const [keyWord, setKeyWord] = useState("");
+  // const onChangeKeyWord = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //   setKeyWord(e.target.value);
+  // };
 
-  const onChangeKeyWord = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setKeyWord(e.target.value);
-  };
+  const [tags, setTags] = useState(9);
+
+  const tagName = 'Go'
 
   return (
     <>
       <Header />
-      <Flex w="80%" justify="space-between" mx="auto" mt={10}>
-        <Input
-          placeholder="プログラミング言語で検索"
-          value={keyWord}
-          onChange={(e) => onChangeKeyWord(e)}
-        />
-        <PrimaryButton>検索</PrimaryButton>
+      <Flex align="center" justify="center" flexDirection="column">
+        {/* <Flex w="80%" justify="space-between" mx="auto" mt={10}>
+          <Input
+            placeholder="キーワード検索は開発中"
+            value={keyWord}
+            onChange={(e) => onChangeKeyWord(e)}
+            disabled={true}
+          />
+          <PrimaryButton>検索</PrimaryButton>
+        </Flex> */}
+        <Heading fontSize="20px" mt="40px">
+          タグで検索
+        </Heading>
+
+        <Flex
+          position="relative"
+          m="2em 0"
+          maxW="960px"
+          w="100%"
+          flexWrap="wrap"
+          justify="space-between"
+          _after={{
+            content: "''",
+            display: "block",
+            width: {
+              md: `calc(${25 * (4 - (tags % 4))}%)`,
+              base: `calc(${50 * (2 - (tags % 2))}%)`,
+            },
+          }}
+        >
+          {Array(tags)
+            .fill(0)
+            .map((_, index) => {
+              return (
+                <Link href={`/tags/${tagName.toLocaleLowerCase()}`} key={index}>
+                  <Box
+                    m={{ md: "0.5em auto", base: "0.5em auto" }}
+                    p="0"
+                    w={{ md: " calc(96%/4)", base: "calc(94%/2)" }}
+                  >
+                    <Box>
+                      <Box
+                        p=".5em 1em"
+                        fontWeight="bold"
+                        color="#6091d3"
+                        bg="#FFF"
+                        border="solid 3px #6091d3"
+                        borderRadius="10px"
+                        textAlign="center"
+                        cursor="pointer"
+                      >
+                        FirebaseFi
+                      </Box>
+                    </Box>
+                  </Box>
+                </Link>
+              );
+            })}
+        </Flex>
       </Flex>
     </>
   );
