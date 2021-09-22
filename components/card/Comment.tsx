0f9894a95b38ce8953aa44760301d18ec7eabe8a
rinka0v0 <rinka0v0@users.noreply.github.com>
2021-09-22 23:17:25 +0900

    [fix]　コメント部分をレスポンシブ対応した

--- components/card/Comment.tsx
+++ components/card/Comment.tsx
@@ -9,7 +9,7 @@ import DOMPurify from "dompurify";
 import marked from "marked";
 import { useContext } from "react";
 import { AuthContext } from "../../auth/AuthProvider";
-import { Button } from "@chakra-ui/react";
+import { Button, HStack } from "@chakra-ui/react";
 import useMessage from "../../hooks/useMessage";
 import useFetchComment from "../../hooks/useFetchComment";
 
@@ -67,33 +67,52 @@ const Comment: React.VFC<Props> = (props) => {
   return (
     <>
       <Flex w="100%">
-        <Box my="1.5em" cursor="pointer">
+        <Box
+          my="1.5em"
+          cursor="pointer"
+          display={{ base: "none", md: "initial" }}
+        >
           <Link href={`/${userId}`}>
             <Avatar src={user?.iconURL} mb={2} />
           </Link>
         </Box>
-        <Box w="100%">
+        <Box w={{ base: "100%", md: "80%" }}>
           <Box
             width="100%"
             display="inline-block"
             position="relative"
-            ml="2em"
+            ml={{ md: "2em" }}
             p="17px 13px"
             borderRadius="12px"
             background="#d7ebfe"
             className="markdown-body"
             _after={{
-              content: '""',
-              position: "absolute",
-              top: "36px",
-              left: "-24px",
-              border: "12px solid transparent",
-              borderRight: "12px solid #d7ebfe",
+              md: {
+                content: '""',
+                position: "absolute",
+                top: "36px",
+                left: "-24px",
+                border: "12px solid transparent",
+                borderRight: "12px solid #d7ebfe",
+              },
             }}
           >
-            <Flex mb={3} align="center">
-              <Box>{user?.user_name}</Box>
-              <Box mx={5}>{createdAt}</Box>
+            <Flex
+              mb={3}
+              align="center"
+              flexDirection={{ base: "column", md: "initial" }}
+            >
+              <HStack align="center">
+                <HStack align="center">
+                  <Box display={{ md: "none" }}>
+                    <Link href={`/${userId}`}>
+                      <Avatar src={user?.iconURL} mb={2} size="sm" />
+                    </Link>
+                  </Box>
+                  <Box>{user?.user_name}</Box>
+                </HStack>
+                <Box>{createdAt}</Box>
+              </HStack>
               {currentUser?.uid === userId ? (
                 <Button
                   colorScheme="red"
