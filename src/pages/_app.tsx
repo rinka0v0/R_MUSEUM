4440838e4776116c33a1b86fbe2651ef80284ca7
rinka0v0 <rinka0v0@users.noreply.github.com>
2021-09-25 22:36:41 +0900

    [clean] リファクタリング

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,5 @@
-import { Box, ChakraProvider, Flex } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
-import Head from "next/head";
-import Footer from "../components/layout/Footer";
 import theme from "../theme/theme";
 import { AuthProvider } from "../auth/AuthProvider";
 
@@ -10,15 +8,7 @@ const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
     <>
       <ChakraProvider theme={theme}>
         <AuthProvider>
-          <Flex flexDirection="column" minH="100vh">
-            <Head>
-              <title>R_MUSEUM</title>
-            </Head>
-            <Box flex="1">
-              <Component {...pageProps} />
-            </Box>
-            <Footer />
-          </Flex>
+          <Component {...pageProps} />
         </AuthProvider>
       </ChakraProvider>
     </>
